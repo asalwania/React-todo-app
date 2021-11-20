1ca57302c6dd23881431b0c08d2ac28e588a1559
ajay.kumar <ajay.kumar@softobiz.com>
2021-11-20 16:22:35 +0530

    added loader

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -6,16 +6,14 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { getDocs } from "@firebase/firestore";
-
 import NavBar from "../toolbar/NavBar";
-import { todosRef } from "../../config/firebase.config";
 import TodoRow from "./TodoRow";
 import AddTodoForm from "../forms/AddTodoForm";
 import { StyledTableCell } from "./todos.styles";
 
 import { useDispatch, useSelector } from "react-redux";
 import { getTodosData } from "../../redux/slices/todos.slice";
+import { CircularProgress } from "@mui/material";
 
 export default function Todos() {
   const dispatch = useDispatch();
@@ -34,24 +32,30 @@ export default function Todos() {
 
   return (
     <>
+      <NavBar />
       <TableContainer component={Paper} sx={{ width: "80%", margin: "auto" }}>
         {/* navbar */}
-        <NavBar />
-        <Table sx={{ minWidth: 700 }} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>Tasks</StyledTableCell>
-              <StyledTableCell align="right">Category</StyledTableCell>
-              <StyledTableCell align="right"></StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {/* todos list */}
-            {todos.map((todo) => (
-              <TodoRow key={todo.id} todo={todo} />
-            ))}
-          </TableBody>
-        </Table>
+        {status === "loading" ? (
+          <div className="section-center-loader">
+            <CircularProgress />
+          </div>
+        ) : (
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Tasks</StyledTableCell>
+                <StyledTableCell align="right">Category</StyledTableCell>
+                <StyledTableCell align="right"></StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {/* todos list */}
+              {todos.map((todo) => (
+                <TodoRow key={todo.id} todo={todo} />
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </TableContainer>
       {/* add todo form */}
       <AddTodoForm />
